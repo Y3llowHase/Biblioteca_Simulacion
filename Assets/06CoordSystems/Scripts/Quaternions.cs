0c4b0ab2e324dfde6370ef2a929b062197ab77be
Y3llowHase <valentina.londonosa@upb.edu.co>
2022-10-08 18:25:06 -0500

    Matrix

---
+++ Assets/06CoordSystems/Scripts/Quaternions.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quaternions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion q = Quaternion.Euler(0, 0, 5 * Time.deltaTime);
+        transform.rotation = transform.rotation * q;
+    }
+}
